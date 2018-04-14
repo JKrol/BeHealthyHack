4a0adce5bd192e32a9af0c14c8d6f9caf6dd7550
Jakub Król <jakub.krol@mc2solutions.pl>
2018-04-14 10:33:06 +0200

    Vuex added

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
 import router from './router'
+import store from './store'
+
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -22,6 +25,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
