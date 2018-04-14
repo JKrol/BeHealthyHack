0722c5f31e38c8ddfffae1adf1b61df4e974e492
Aleksander Surman <aleksander.surman@mc2solutions.pl>
2018-04-14 22:29:30 +0200

    Added Indico sample queries

---
+++ src/components/IndicoTest.vue
@@ -0,0 +1,39 @@
+<template>
+</template>
+
+<script>
+// import smutny from "../assets/smutny.jpg"
+// import wesoly from "../assets/wesoly.jpg"
+import indico from "../modules/indico"
+
+export default {
+  mounted(){
+      // indico.getSentimentFromImg(wesoly).then(data => {
+      //   console.log("wesoly");
+      //   console.log(data);
+      // }).catch(err => {console.log(err);});
+
+      // indico.getSentimentFromImg(smutny).then(data => {
+      //   console.log("smutny");
+      //   console.log(data);
+      // }).catch(err => {console.log(err);});
+      
+
+      ["Hi, i'm fap and i like to masturbate!",
+      "I hate my work.",
+      "Why do I dont have any friends.",
+      "Im wondering if tomorrow will be nice weather.",
+      "I am very sleepy after this whole day of work",
+      "I am so fucked up. Please message me."].forEach(function(txt){
+      indico.getSentimentFromText(txt).then(data => {
+        console.log(txt);
+        console.log(data);
+      }).catch(err => {console.log(err);});
+      });
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import IndicoTest from '@/components/IndicoTest'
 import TextToSpeechTest from '@/components/TextToSpeechTest'
 
 Vue.use(Router)
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/IndicoTest',
+      name: 'IndicoTest',
+      component: IndicoTest
     },
     {
       path: '/TextToSpeechTest',
