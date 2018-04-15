5ecb6414713385454829d3f974cbd18466050711
Jakub Król <jakub.krol@mc2solutions.pl>
2018-04-15 08:58:10 +0200

    Firebase configuration added

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -25,7 +25,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+      "Access-Control-Allow-Headers": "X-Requested-With, Authorization"
     },
     
     clientLogLevel: 'warning',
