fdb7fa454da3b3a19946b474ec2d10f01f62b50b
Patryk Lizoń <patryk@hangover.studio>
2018-04-15 06:17:03 +0200

    Add SolidInput component

---
+++ src/components/SolidInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <input class="solid-input" type="text" :placeholder="placeholder" :name="name" :v-model="model" />
+</template>
+
+<script>
+export default {
+  name: "solid-input",
+  props: ["placeholder", "name", "model"]
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/config/index.scss";
+
+.solid-input {
+  background-color: $color-white;
+  border-radius: 4px;
+  box-shadow: $box-shadow-default;
+  color: $color-black;
+  padding: 12px 16px;
+  width: 100%;
+  outline: none;
+}
+</style>
