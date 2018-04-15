f6a0c7205e0fa39ea7bcf86e48091b6a9a4566da
Patryk Lizoń <patryk@hangover.studio>
2018-04-15 10:34:03 +0200

    Change Main view component

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,66 +1,62 @@
 <template>
-  <div>
-    Main page
-    <ul>
-      <li v-for="friend in friends" :key="friend.id">
-        <img :src="friend.picture" @click="selectFriend(friend.id)" />
-      </li>
-    </ul>
-    <br/>
-    <br/>
+  <section class="main view">
+    <friend-chooser :friends="friends" :onClick="selectFriend" />
+
     <div v-if="selectedFriend">
       <friend-card :friend="selectedFriend" />
-      <br/>
-      <br/>
       <friend-timeline :friend="selectedFriend" />
     </div>
-  </div>
+    <bottom-menu />
+  </section>
 </template>
 
 <script>
-import FriendCard from '@/components/FriendCard'
-import FriendTimeline from '@/components/FriendTimeline'
+import FriendCard from "../components/FriendCard";
+import FriendTimeline from "../components/FriendTimeline";
+import FriendChooser from "../components/FriendChooser";
+import BottomMenu from "../components/BottomMenu";
 
 export default {
   components: {
-    FriendCard, FriendTimeline
+    FriendChooser,
+    FriendCard,
+    FriendTimeline,
+    BottomMenu
   },
   methods: {
     selectFriend(friendId) {
-      this.$router.push({ name: 'main_selected', params: { id: friendId }})
+      this.$router.push({ name: "main_selected", params: { id: friendId } });
     }
   },
   data() {
     return {
       selected: null
-    }
+    };
   },
   computed: {
     friends() {
       return this.$store.getters.userSelectedFriends;
     },
     selectedFriend() {
-      if(!this.friends)
-        return null;
+      if (!this.friends) return null;
 
       var id = this.$route.params.id;
-      if(!id && this.friends)
-        id = this.friends[0].id;
+      if (!id && this.friends) id = this.friends[0].id;
 
-      if(!id)
-        return null;
+      if (!id) return null;
 
-      for(var i=0; i<this.friends.length; i++) {
-        if(this.friends[i].id == id)
-          return this.friends[i];
+      for (var i = 0; i < this.friends.length; i++) {
+        if (this.friends[i].id == id) return this.friends[i];
       }
-      
+
       return null;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-
+.main {
+  padding-bottom: 120px;
+}
 </style> 
