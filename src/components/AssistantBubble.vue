717fd0150e6c895ee76bc314c6a2b2f110163b0b
Patryk Lizoń <patryk@hangover.studio>
2018-04-15 03:24:49 +0200

    Add assistant bubble component

---
+++ src/components/AssistantBubble.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class='["assistant-bubble", message]'>
+    <div v-html="pulse" class="pulse" />
+    <message-bubble :message='message' theme="t-white" direction='in' />
+  </div>
+</template>
+
+<script>
+import MessageBubble from './MessageBubble';
+
+import pulse from "../assets/pulse.svg";
+
+export default {
+  name: 'assistant-bubble',
+  props: ["message"],
+  components: {
+    MessageBubble
+  },
+
+  data() {
+    return {
+      pulse
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/config/index.scss";
+
+.assistant-bubble {
+  display: flex;
+  align-items: flex-end;
+  width: 100%;
+
+  .pulse {
+    height: 80px;
+    margin-right: 16px;
+    width: 80px;
+  }
+
+  .message-bubble {
+    margin-bottom: 6px;
+  }
+}
+</style>
